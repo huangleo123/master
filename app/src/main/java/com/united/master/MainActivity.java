31a8a5f362f1780c264c99c33969f744c4330b46
huangleo123@hotmail.com <a159753>
2017-07-15 20:59:39 +0800

    修改了第一次

--- app/src/main/java/com/united/master/MainActivity.java
+++ app/src/main/java/com/united/master/MainActivity.java
@@ -2,12 +2,15 @@ package com.united.master;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
